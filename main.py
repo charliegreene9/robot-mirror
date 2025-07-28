86d164bd4b0c636c949c58670ff68dcb0f21d9d6
charliegreene9 <charliegreene9@gmail.com>
2025-07-28 16:44:38 +0200

    V 0.2.0 Extension of main, functions written out, gitignore included, more test files attached. - Main now setup for most of first phase - function for finding the difference added - LAst scenario added to rescale function - function for checking if cropped rewritten to be generic - .gitignore updated to avoid adding notebooks used for testing/writing
    ** TODO **
    - Check whole flow works
    - Run a test for each type of distortion
    - Make sure prints are included & w/ appropriate info where needed
    - Create standard output to compare the images and include highlighted changes
    - Plan the next distortion checks (combination)

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ In particular it can be used in tests where the output of a
 function is an image.
 """
 
-from pathlib import Path
-
 import cv2
 import numpy as np
 from PIL import Image
 
 
-def rescale_image(img_1: str | Path, img_2: str | Path):
+def rescale_image(img_1: Image, img_2: Image):
     # Check the image size, two paths from there - resize, check if cropped
     width_1, height_1 = img_1.size
     width_2, height_2 = img_2.size
@@ -23,21 +21,61 @@ def rescale_image(img_1: str | Path, img_2: str | Path):
         print("Only the height has been changed.")
     elif width_ratio != 1 and height_ratio == 1:
         print("Only the width has been changed.")
+    else:
+        print(f"Image has been rescaled by a factor of: {width_ratio}")
     return img_2.resize((width_1, height_1))
 
 
-def check_if_cropped(img_1, img_2):
-    img_rgb = cv2.imread("mario.png")
-    template = cv2.imread("mario_coin.png")
-    w, h = template.shape[:-1]
+def check_if_cropped(img_1: np.array, img_2: np.array):
+    # Find which image is smaller
+    if img_1.shape[0] <= img_2.shape[0] and img_1.shape[1] <= img_2.shape[1]:
+        img_cropped = img_1
+        img_full = img_2
+    elif img_1.shape[0] >= img_2.shape[0] and img_1.shape[1] >= img_2.shape[1]:
+        img_cropped = img_2
+        img_full = img_1
+    else:
+        print("Image seems to be reshaped or distorted.")
+        return None
 
-    res = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
+    w, h = img_cropped.shape[:-1]
+
+    res = cv2.matchTemplate(img_full, img_cropped, cv2.TM_CCOEFF_NORMED)
     threshold = 0.8
     loc = np.where(res >= threshold)
     for pt in zip(*loc[::-1], strict=False):  # Switch columns and rows
-        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+        cv2.rectangle(img_full, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
 
-    cv2.imwrite("result.png", img_rgb)
+    cv2.imwrite("highlight_crop.png", img_full)
+
+    if len(loc) > 0:
+        return True
+    else:
+        return False
+
+    # img_rgb = cv2.imread("mario.png")
+    # template = cv2.imread("mario_coin.png")
+    # w, h = template.shape[:-1]
+
+    # res = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
+    # threshold = 0.8
+    # loc = np.where(res >= threshold)
+    # for pt in zip(*loc[::-1], strict=False):  # Switch columns and rows
+    #     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+
+    # cv2.imwrite("result.png", img_rgb)
+
+
+def find_difference(img_1: np.array, img_2: np.array):
+    # Making a blank array in the shape of img_2
+    out = np.zeros_like(img_2)
+    # Finding where the difference is
+    mask = img_1 != img_2
+    out[mask] = img_2[mask]
+    # Converting back to image, save & return
+    out_img = Image.fromarray(out)
+    out_img.save("differences_1.jpg")  # TODO add naming convention
+    return out_img
 
 
 # Compare two images pixel by pixel
@@ -49,7 +87,7 @@ if img_1.shape == img_2.shape and np.all(img_1 == img_2):
     print("Images are identical!")
 else:
     print("Images are different.")
-
+print(img_1.shape)
 out = np.zeros_like(img_2)
 
 mask = img_1 != img_2
@@ -90,13 +128,25 @@ def main(img_1_loc, img_2_loc):
         img_1_array == img_2_array
     ):
         print("Images are identical!")
+        return None
+    elif img_1_array.shape == img_2_array.shape and np.all(
+        img_1_array != img_2_array
+    ):
+        print("Difference has been detected. Identifying changes...")
+        img_diff = find_difference(img_1, img_2)
+        return img_diff
     else:
-        print("Images are different.")
-
+        print("""Image has a different shape.
+              Checking for cropping or reshaping...""")
     # If the size is the issue then check if it is cropped
-
-    # If not cropped check if it can rescale
-
+    if check_if_cropped(img_1_array, img_2_array):
+        return None  # check logic here
+    else:
+        # If not cropped check if it can rescale
+        rescaled_img = rescale_image(img_1, img_2)
+        rescaled_img.save("Rescaled_result.jpg")
+    # Look at combo changes, e.g. edit and rescale
+    # Drop threshold in crop check, then apply the diff check to match
     # If not find the differences
 
     # Categorise the difference if possible, e.g. text, noise, compressed
